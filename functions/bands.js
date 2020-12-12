a5d569f4fac58ca3949a0e1155bb76f5986fa3a0
Juan Rinconada <jrinconada@gmail.com>
2020-12-12 16:00:40 +0100

    Add new databases and other get functions and update docs

---
+++ functions/bands.js
@@ -0,0 +1,9 @@
+
+const bands = require('./bands.json');
+
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(bands)
+  });
+};
---
+++ functions/trapi.js
@@ -0,0 +1,9 @@
+
+const trappers = require('./trappers.json');
+
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(trappers)
+  });
+};
